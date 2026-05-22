4a88706879da4f9a405cdd26fc1d1456f07e7ddc
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-05-22 09:48:17 +0900

    2-1c

--- code/2-1.c
+++ code/2-1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char* argv[], const char* envp[]) {
+int main(int argc, const char *argv[], const char *envp[]) {
+    FILE* fp = fopen("student.txt", "w");
 
+    while (1) {
+        char name[10] = {0};
+        int score = 0;
+        scanf("%s", name);
+        if (strcmp(name, "quit") == 0) {
+            break;
+        }
+        scanf("%d", &score);
+
+        fprintf(fp, "%s %d\n", name, score);
+    }
+    fclose(fp);
     return 0;
 }
-
