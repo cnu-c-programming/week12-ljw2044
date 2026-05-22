bfb34f649147db2687fb17d7d3e9f0c6f7f16434
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-05-22 09:52:33 +0900

    2-2c

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,28 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
+    if (fp == NULL)
+        return 0;
 
+    while (fscanf(fp, "%s %d", students[count].name, &students[count].score) != EOF) {
+        count++;
+    }
 
     int max = 0;
     float avg = 0;
 
-
+    if (count > 0) {
+        int sum = 0;
+        max = students[0].score;
+        
+        for (int i = 0; i < count; i++) {
+            sum += students[i].score;
+            if (students[i].score > max) {
+                max = students[i].score;
+            }
+        }
+        avg = (float)sum / count;
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
@@ -25,4 +41,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-
