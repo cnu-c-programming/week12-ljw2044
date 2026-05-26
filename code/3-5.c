6a4d67248f530f0a0ca44bd837da8f774206293b
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-05-26 10:32:24 +0900

    3-5c

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,24 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    char line[128];
 
+    while (fgets(line, sizeof(line), fp)) {
+        int val;
+        if (sscanf(line, "%d", &val) == 1) {
+            sum += val;
+        } else {
+            if (line[0] != '\n' && line[0] != '\r' && line[0] != '\0') {
+                int len = 0;
+                while (line[len] != '\n' && line[len] != '\r' && line[len] != '\0') {
+                    len++;
+                }
+                line[len] = '\0';
+                fprintf(stderr, "invalid input %s\n", line);
+            }
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
